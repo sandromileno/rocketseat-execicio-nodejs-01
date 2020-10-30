7cc8f3e61336403784ddd4d83814807693c1fed0
Sandro Mileno <sandro.mileno@gmail.com>
2020-10-30 18:45:50 -0300

    Adding solution

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid, v4 } = require("uuid");
 
 const app = express();
 
@@ -11,23 +11,60 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const repository = ({ title, url, techs } = request.body);
+  repository.id = uuid();
+  repository.likes = 0;
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repository = repositories.find((r) => r.id === id);
+
+  if (repository) {
+    repository.title = title;
+    repository.url = url;
+    repository.techs = techs;
+    return response.status(200).send(repository);
+  }
+  return response.status(400).send();
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex((r) => r.id === id);
+
+  console.log(index);
+
+  if (index < 1) {
+    return response.status(400).send();
+  }
+
+  repositories.splice(index, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find((r) => r.id === id);
+
+  if (repository) {
+    repository.likes++;
+    return response.status(201).send(repository);
+  }
+  return response.status(400).send();
 });
 
 module.exports = app;
